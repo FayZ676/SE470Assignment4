51d9b03ab684cb993efcb73dba609a673131547d
Faizi Fifita <94952795+FayZ676@users.noreply.github.com>
2024-02-07 11:25:07 -0800

    add failing test.

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,5 +5,9 @@ class TestAddition(unittest.TestCase):
   def test_add(self):
     self.assertEqual(add(2,3),5)
 
+  def test_add_fail(self):
+          # Intentionally failing test
+          self.assertEqual(add(2, 3), 6)
+
 if __name__ == "__main__":
   unittest.main()
