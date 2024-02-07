3fcf33257565ad1650f34360d649eaa764705f87
Faizi Fifita <94952795+FayZ676@users.noreply.github.com>
2024-02-07 11:38:33 -0800

    add mistune and test.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import mistune
+
 def add(a,b):
   return a + b
 
+def convert_markdown_to_html(markdown: str):
+  return mistune.html(markdown)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,18 @@
 import unittest
-from main import add
+from main import add, convert_markdown_to_html
 
 class TestAddition(unittest.TestCase):
   def test_add(self):
     self.assertEqual(add(2,3),5)
 
   def test_add_fail(self):
-          # Intentionally failing test
-          self.assertEqual(add(2, 3), 6)
+    # Intentionally failing test
+    self.assertEqual(add(2, 3), 6)
+
+  def test_mistune(self):
+      sample_markdown = "**This is [a link with *nested* emphasis](https://example.com)**"
+      self.assertEqual(convert_markdown_to_html(sample_markdown), 
+                       '<p><strong>This is <a href="https://example.com"><em>a link with <strong>nested</strong> emphasis</em></a></strong></p>')
 
 if __name__ == "__main__":
   unittest.main()
