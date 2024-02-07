f92e85e83da1c638ed7c3e9adf5e65015869c0a5
Faizi Fifita <94952795+FayZ676@users.noreply.github.com>
2024-02-07 11:43:11 -0800

    add more basic test for mistune.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,7 @@
 import unittest
 from main import add, convert_markdown_to_html
 
-class TestAddition(unittest.TestCase):
+class TestMain(unittest.TestCase):
   def test_add(self):
     self.assertEqual(add(2,3),5)
 
@@ -10,9 +10,9 @@ class TestAddition(unittest.TestCase):
     self.assertEqual(add(2, 3), 6)
 
   def test_mistune(self):
-      sample_markdown = "**This is [a link with *nested* emphasis](https://example.com)**"
+      sample_markdown = "# Hello world"
       self.assertEqual(convert_markdown_to_html(sample_markdown), 
-                       '<p><strong>This is <a href="https://example.com"><em>a link with <strong>nested</strong> emphasis</em></a></strong></p>')
+                       '<h1>Hello world</h1>')
 
 if __name__ == "__main__":
   unittest.main()
